60bbd2a6fc0307494e6971d9314c5ff035de4fdc
Eduardo Alves <eduardonalves@gmail.com>
2015-11-30 21:39:12 -0200

    ajuste cadastro 002

--- js/funcoes.js
+++ js/funcoes.js
@@ -4134,6 +4134,7 @@ var getBairroFromCep=null;
     salt ="jmgl33mg1221kjgruyky232ho2l3437mhljio90hueemmgjktjmmmgko2tut35ymmmh221eenngl4y73kkkj";
     $(document).on( "pageshow",'#page5', function() {
         $.mobile.loading( "hide" );
+        $("cadastroContent").hide();
         atualizarLojas();
 
         $('.empresaEdit').val(empresa);
